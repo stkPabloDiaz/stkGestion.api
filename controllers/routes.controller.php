5a774cc72f5e2767e5c906743232fc8dccdc5331
Strockersoft <stkpablodiaz@gmail.com>
2023-06-04 23:46:59 -0300

    first commit

---
+++ controllers/routes.controller.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class RoutesController{
+
+	/*=============================================
+	Ruta Principal
+	=============================================*/
+	public function index(){
+		include "routes/routes.php";
+	}
+
+
+    
+}
